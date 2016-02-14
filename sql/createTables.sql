ee04c6dfdd7bd980d9f82aaf032e7a604c823dc2
Eric Lefort <leforte@mcmaster.ca>
2016-02-13 19:19:15 -0500

    Created initial SQL table code, feedback is encouraged

---
+++ sql/createTables.sql
@@ -0,0 +1,48 @@
+CREATE TABLE Rock(
+	`RockID` INT,
+	`Name` VARCHAR(255),
+	`Value` NUMERIC(15, 2) NOT NULL,
+	`Colour` VARCHAR(255) DEFAULT `UNKNOWN`,
+	`Texture` VARCHAR(255) DEFAULT `UNKNOWN`,
+	`Size` VARCHAR(255) DEFAULT `UNKNOWN`,
+
+	PRIMARY KEY `RockID`
+);
+
+CREATE TABLE User(
+	`Password` VARCHAR(255) NOT NULL,
+	`HistoryID` INT,
+
+	PRIMARY KEY `Password`
+);
+
+CREATE TABLE History(
+	`HistoryID` INT,
+	`RockID` INT NOT NULL,
+	`Date` DATE DEFAULT GETDATE() NOT NULL,
+
+	FOREIGN KEY(`RockID`) REFERENCES Rock(`RockID`) ON DELETE CASCADE,		#Should delete all history items referencing the deleted rock.
+	PRIMARY KEY('HistoryID')
+);
+
+CREATE TABLE Location(
+	`Lat` INT,
+	`Lon` INT,
+
+	PRIMARY KEY (`Lat`, `Lon`)
+);
+
+#INSERT all rock data here.
+#INSERT INTO Rock VALUES(0,'UNKNOWN');
+
+#TODO ensure validity of this method, it is new to me.
+delimiter $$					#Temporarily updates the delimiter to '$$' so I can use ';' within the trigger.
+
+CREATE TRIGGER updateHistory
+BEFORE UPDATE ON History
+FOR EACH ROW 
+BEGIN
+	SET NEW.Date = OLD.Date;	#Forces all dates to remain unchanged.
+END$$
+
+delimiter;						#Restores the delimiter.
