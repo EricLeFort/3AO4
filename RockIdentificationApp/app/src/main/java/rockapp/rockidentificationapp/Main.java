4e3f172ca3dcc03f1a1cf2e75cc12701732b3af8
Nikolai Savas <nikosavas@hotmail.com>
2016-04-01 19:23:53 -0400

    Built main page spinners, connected

--- RockIdentificationApp/app/src/main/java/rockapp/rockidentificationapp/Main.java
+++ RockIdentificationApp/app/src/main/java/rockapp/rockidentificationapp/Main.java
@@ -2,12 +2,37 @@ package rockapp.rockidentificationapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
 
 public class Main extends AppCompatActivity {
+
+    private Spinner hardnessSelect;
+    private Spinner colourSelect;
+    private Spinner textureSelect;
+    private Spinner sizeSelect;
+    private CheckBox useLocationChkBox;
+    private Button historyBtn;
+    private Button exitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Connect private members
+        connectSpinner(hardnessSelect, R.id.hardness_select, R.array.hardness_array);
+        connectSpinner(colourSelect, R.id.colour_select, R.array.colour_array);
+        connectSpinner(textureSelect, R.id.texture_select, R.array.texture_array);
+        connectSpinner(sizeSelect, R.id.size_select, R.array.size_array);
+    }
+
+    private void connectSpinner(Spinner spinner, int viewId, int stringResourceId) {
+        spinner = (Spinner) findViewById(viewId);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, stringResourceId, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
