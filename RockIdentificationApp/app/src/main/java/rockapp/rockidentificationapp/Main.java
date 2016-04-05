c864c2e5750a9b47bf36bfe26272f46a55fbf3c3
Nikolai Savas <nikosavas@hotmail.com>
2016-04-05 11:21:42 -0400

    Been programming too much in C++... Java doesn't pass by reference

--- RockIdentificationApp/app/src/main/java/rockapp/rockidentificationapp/Main.java
+++ RockIdentificationApp/app/src/main/java/rockapp/rockidentificationapp/Main.java
@@ -25,18 +25,19 @@ public class Main extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Connect private members
-        connectSpinner(hardnessSelect, R.id.hardness_select, R.array.hardness_array);
-        connectSpinner(colourSelect, R.id.colour_select, R.array.colour_array);
-        connectSpinner(textureSelect, R.id.texture_select, R.array.texture_array);
-        connectSpinner(sizeSelect, R.id.size_select, R.array.size_array);
+        hardnessSelect = connectSpinner(hardnessSelect, R.id.hardness_select, R.array.hardness_array);
+        colourSelect = connectSpinner(colourSelect, R.id.colour_select, R.array.colour_array);
+        textureSelect = connectSpinner(textureSelect, R.id.texture_select, R.array.texture_array);
+        sizeSelect = connectSpinner(sizeSelect, R.id.size_select, R.array.size_array);
     }
 
     //Sync up the string resource, dropdown in the xml, and this class
-    private void connectSpinner(Spinner spinner, int viewId, int stringResourceId) {
+    private Spinner connectSpinner(Spinner spinner, int viewId, int stringResourceId) {
         spinner = (Spinner) findViewById(viewId);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, stringResourceId, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        return spinner;
     }
 
     public void historyClicked(View view){
