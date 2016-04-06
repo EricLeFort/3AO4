9be15c05bf9c168deefbfda5c302a9fece12de16
Nicholas Lago <lagond@mcmaster.ca>
2016-04-06 10:53:34 -0700

    Cipher up and running!
    Block cipher using AES, Key generation optional (needed on first run).
    Using padding pkcs5. Passing a message returns a byte[], decoding a
    message takes in that byte[], key and IV and returns original string.
    Use global Key for program, make sure its private

--- Rock_Identifier/Message_Passer.java
+++ Rock_Identifier/Message_Passer.java
@@ -1,4 +1,4 @@
-package tools;
+package test;
 
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
@@ -13,22 +13,28 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 
-import sun.misc.BASE64Encoder;
+import sun.misc.*;
 
 /**
  * @author Nicholas Lago
  * @version 1.0
  */
 public class Message_Passer{
-private keySize = 128; // 16 bytes
-private byte[] v = new byte[keySize/8]; //from bits to bytes
-private SecretKey key; // symmetric key used
+    private int keySize = 128; // 16 bytes
+    private byte[] v = new byte[keySize/8]; //from bits to bytes
+    private SecretKey key; // symmetric key used
 
     public byte[] messagePass(String message){
-        encode(message);
+        return encode(message, null);
+    }
+    public byte[] messagePass(String message, SecretKey sKey){
+        return encode(message, sKey);
     }
 
-    private byte[] encode(String message){
+
+
+    private byte[] encode(String message, SecretKey sKey){
+        byte[] empty = new byte [1];
         try{
             /** AES Key Generator
              * AES - Advanced Encryption Standard
@@ -37,9 +43,12 @@ private SecretKey key; // symmetric key used
              * that uses the algorithm specified
              */
             KeyGenerator gen = KeyGenerator.getInstance("AES");
-            gen.init(keySize);
-            key = gen.generateKey(); // key created
-
+            if (sKey == null){
+                gen.init(keySize);
+                key = gen.generateKey(); // key created
+            }else{
+                key = sKey;
+            }
             //Initialization Vector
             // PRNG - Pseudo random number generator
             SecureRandom PRNG = new SecureRandom();
@@ -51,7 +60,8 @@ private SecretKey key; // symmetric key used
              * is XOR'd with cipher text.
              * PKCS7 pads with whole bytes. Each byte is the value
              */
-            Cipher bill= Cipher.getInstance("AES/CBC/PKCS7PADDING");
+            Cipher bill= Cipher.getInstance("AES/CBC/PKCS5PADDING");
+
             /**
              * Cipher is initialized to encryption mode, IVParameterSpec allows the
              * cipher to use the vector v as an IV
@@ -59,41 +69,68 @@ private SecretKey key; // symmetric key used
             bill.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(v));
 
             byte[] byteMe = message.getBytes();
-            byte[] byteBill = bill.dofinal(byteMe); // pass in the message in byte form
+            byte[] byteBill = bill.doFinal(byteMe); // pass in the message in byte form
 
 
             return byteBill;
 
         }catch (NoSuchAlgorithmException alg){
-            return "";
+            alg.printStackTrace();
+            return empty;
         }catch (NoSuchPaddingException pad) {
-            return "";
+            System.out.println("No such Padding");
+            return empty;
         }catch (InvalidKeyException invKey){
-            return "";
+            System.out.println("Invalid Key");
+            return empty;
         }catch (InvalidAlgorithmParameterException parm){
-            return "";
+            System.out.println("Invalid algo Parameter");
+            return empty;
         }catch (IllegalBlockSizeException blockSize){
-            return "";
+            System.out.println("Illegal Block Size");
+            return empty;
+        }catch(BadPaddingException padding){
+            System.out.println("Bad Padding");
+            return empty;
         }
     }
 
     public String messageDecode(byte[] message, SecretKey sKey, byte[] vect){
-        decode(message,skey, vect );
+        return decode(message,sKey, vect );
     }
 
     private String decode(byte[] message, SecretKey sKey, byte[] vect){
-
+        String empty = "";
         try{
             // just like the encryption process ..
-            Cipher bill = Cipher.getInstance("AES/CBC/PKCS7PADDING");
-            bill.init(Cipher.DECRYPT_MODE, sKey, IvParameterSpec(vect));
+            Cipher bill = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            bill.init(Cipher.DECRYPT_MODE, sKey, new IvParameterSpec(vect));
             byte[] byteMe = bill.doFinal(message);
             return new String(byteMe);
 
+        }catch (NoSuchAlgorithmException alg){
+            alg.printStackTrace();
+            return empty;
+        }catch (NoSuchPaddingException pad) {
+            System.out.println("No such Padding");
+            return empty;
+        }catch (InvalidKeyException invKey){
+            System.out.println("Invalid Key");
+            return empty;
+        }catch (InvalidAlgorithmParameterException parm){
+            System.out.println("Invalid algo Parameter");
+            return empty;
+        }catch (IllegalBlockSizeException blockSize){
+            System.out.println("Illegal Block Size");
+            return empty;
+        }catch(BadPaddingException padding){
+            System.out.println("Bad Padding");
+            return empty;
         }
-
     }
-    public secretKey getKey(){
+
+
+    public SecretKey getKey(){
         return key;
     }
     public byte[] getIV(){
@@ -101,18 +138,22 @@ private SecretKey key; // symmetric key used
     }
 
 
+    /**
+     *     public static void main(String ... args){
+     Message_Passer MP = new Message_Passer();
+     byte[] message = MP.messagePass("Sup");
+     SecretKey mKey = MP.getKey();
+     byte[] vectorBar = MP.getIV();
+     Message_Passer Dec = new Message_Passer();
+     String MESS = Dec.messageDecode(message, mKey, vectorBar);
+     System.out.println(MESS);
+     System.out.println(new String(message));
+
+     }
+     *
+     */
 
 
 
 }
 
-    public static void main(String[] args){
-        Message_Passer MP = new Message_Passer();
-        byte[] message = MP.messagePass("Sup");
-        SecretKey mKey = MP.getKey();
-        byte[] vectorBar = MP.getIV();
-        Message_Passer Dec = new Message_Passer();
-        String MESS = Dec.messageDecode(message, mKey, vectorBar);
-        System.out.println(MESS);
-
-    }
